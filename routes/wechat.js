61b65a83231cc5d696189575717bb657191679e6
xiaoyue26 <296671657@qq.com>
2016-10-04 21:18:03 +0800

    repeat

--- routes/wechat.js
+++ routes/wechat.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var wechat = require('wechat');
-var fs = require('fs');
-var path = require('path');
+
 var config = {
     token: '10000zhongqingdao',
     appid: 'wx3d6d724f53e423da',
@@ -13,48 +12,8 @@ router.use(express.query());
 router.use(wechat(config, function (req, res, next) {
     // message is located in req.weixin
     var message = req.weixin;
-    for(var i in eval(req)){
-        console.log(i);
-    }
-    fs.writeFile(path.join(__dirname, 'users.out'), message,function (err) {
-        if (err) throw err;
-        ob=eval(message);
-        for(var i in ob){
-            console.log(i);
-        }
-
-    });
-    if (message.FromUserName != 'diaosi') {
-        // reply with text
-        res.reply('hehe');
-    } else if (message.FromUserName === 'text') {
-        // another way to reply with text
-        res.reply({
-            content: 'text object',
-            type: 'text'
-        });
-    } else if (message.FromUserName === 'hehe') {
-        // reply with music
-        res.reply({
-            type: "music",
-            content: {
-                title: "Just some music",
-                description: "I have nothing to lose",
-                musicUrl: "http://mp3.com/xx.mp3",
-                hqMusicUrl: "http://mp3.com/xx.mp3"
-            }
-        });
-    } else {
-        // reply with thumbnails posts
-        res.reply([
-            {
-                title: 'Come to fetch me',
-                description: 'or you want to play in another way ?',
-                picurl: 'https://avatars2.githubusercontent.com/u/1942320',
-                url: 'http://121.42.40.161:4000/'
-            }
-        ]);
-    }
+    console.log(message);
+    res.reply(message.Content);
 }));
 /* GET users listing. */
 router.get('/', function (req, res, next) {
