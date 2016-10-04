fb8295fca43219e50af2ce473d03c9a3e4f287c9
xiaoyue26 <296671657@qq.com>
2016-10-04 20:09:21 +0800

    add wechat

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var wechat = require('./routes/wechat');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/wechat', wechat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/wechat.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var wechat = require('wechat');
+
+router.use(wechat('10000zhongqingdao', function (req, res, next) {
+    // message is located in req.weixin
+    var message = req.weixin;
+    res.reply('hehe');
+    if (message.FromUserName === 'diaosi') {
+        // reply with text
+        res.reply('hehe');
+    } else if (message.FromUserName === 'text') {
+        // another way to reply with text
+        res.reply({
+            content: 'text object',
+            type: 'text'
+        });
+    } else if (message.FromUserName === 'hehe') {
+        // reply with music
+        res.reply({
+            type: "music",
+            content: {
+                title: "Just some music",
+                description: "I have nothing to lose",
+                musicUrl: "http://mp3.com/xx.mp3",
+                hqMusicUrl: "http://mp3.com/xx.mp3"
+            }
+        });
+    } else {
+        // reply with thumbnails posts
+        res.reply([
+            {
+                title: 'Come to fetch me',
+                description: 'or you want to play in another way ?',
+                picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+                url: 'http://nodeapi.cloudfoundry.com/'
+            }
+        ]);
+    }
+}));
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.send('into wechat');
+});
+
+module.exports = router;
